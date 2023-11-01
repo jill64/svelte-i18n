1a598fc12f817f00edc555f065efba8680765366
jill64 <intents.turrets0h@icloud.com>
2023-11-01 20:45:44 +0900

    feat: LanguageManager component

---
+++ src/lib/LanguageManager.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { browser } from '$app/environment'
+  import { store } from './store'
+
+  $: ({ locale } = $store)
+
+  $: if (browser) {
+    document.getElementsByTagName('html')[0].setAttribute('lang', $locale)
+  }
+</script>
