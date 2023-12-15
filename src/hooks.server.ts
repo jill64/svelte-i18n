70384dcf727dc9fba69a226bf5d4036e488d460d
jill64 <intents.turrets0h@icloud.com>
2023-12-15 17:25:17 +0900

    update hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,11 @@
-import { attach } from './i18n'
+import { hooks } from '@jill64/npm-demo-layout'
 import { init } from '@jill64/sentry-sveltekit-cloudflare/server'
+import { sequence } from '@sveltejs/kit/hooks'
+import { attach } from './i18n'
 
 const { onHandle, onError } = init(
   'https://574342583bad12b56fa1dbd4faa5305b@o4505814639312896.ingest.sentry.io/4506394548699136'
 )
 
-export const handle = onHandle(attach)
+export const handle = onHandle(sequence(attach, hooks))
 export const handleError = onError()
