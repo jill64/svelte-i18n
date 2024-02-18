7daf6cc898f6d7d1098d34f8b8b5854a39e52493
jill64 <intents.turrets0h@icloud.com>
2024-02-18 13:34:38 +0900

    update docs

--- src/lib/types/Options.ts
+++ src/lib/types/Options.ts
@@ -1,5 +1,19 @@
 export type Options<Locale extends string> = {
+  /**
+   * The slug to be used to change the locale.
+   * @example '[locale]'
+   */
   slug: `[${string}]` | `[${string}=${string}]`
+
+  /**
+   * An array of locales.
+   * @example ['en', 'es', 'fr']
+   */
   locales: Locale[]
+
+  /**
+   * The default locale.
+   * @example 'en'
+   */
   defaultLocale?: Locale
 }
